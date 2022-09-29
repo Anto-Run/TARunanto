013154be725521ae0ec2e8276643ad774f6f7467
Runanto <60428139+Anto-Run@users.noreply.github.com>
2022-09-29 23:15:54 +0700

    update runner

--- TARunanto/src/main/java/com/sqa/psikotes/framework/runner/TestRunner.java
+++ TARunanto/src/main/java/com/sqa/psikotes/framework/runner/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-features = {"src/main/resources/features/00001LPLoginValid.feature", 
-		"src/main/resources/features/00002APOpenPageAdminValid.feature"},
+features = {"src/main/resources/features/001LPLoginValid.feature", 
+		"src/main/resources/features/051APOpenPageAdminValid.feature"},
 glue = "com.sqa.psikotes.framework.runner",
 monochrome=true,
 plugin = {"pretty","html:target/psikotes/htmlreport/PSIKOTESRunner.html",
