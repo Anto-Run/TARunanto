40a26a0a5b36d06ad5da83f28592cbdcf7a44ddf
Runanto <60428139+Anto-Run@users.noreply.github.com>
2022-10-09 19:14:08 +0700

    update project fix

--- TARunanto/src/main/java/com/sqa/psikotes/framework/runner/TestRunner.java
+++ TARunanto/src/main/java/com/sqa/psikotes/framework/runner/TestRunner.java
@@ -4,7 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-features = {"src/main/resources/features/001LPLoginValid.feature",
+features = {
+		"src/main/resources/features/001LPLoginValid.feature",
 		"src/main/resources/features/005LPLoginInvalid.feature",
 		
 		"src/main/resources/features/051APOpenPageAdminValid.feature",
